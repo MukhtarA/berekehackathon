d3a0fbd15506c0ce25b5fa3f83ff75612c50ccd2
baikesha <yerkebulan.dyussembayev@kcell.kz>
2023-07-21 13:59:24 +0600

    add demo screen

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { ThemeProvider } from '@emotion/react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import {useMobileConfigs} from './utils/mobile-actions'
-import { ButtonPrimary,  IconLoader,  Typography } from "./components";
-import themeColors from './components/styles/theme-colors'
-import * as common from './assets/common'
-import DemoScreen from './features/demo-screen';
-import ComponentsScreen from './features/components-screen';
-
+import { ThemeProvider } from "@emotion/react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useMobileConfigs } from "./utils/mobile-actions";
+import { ButtonPrimary, IconLoader, Typography } from "./components";
+import themeColors from "./components/styles/theme-colors";
+import * as common from "./assets/common";
+import DemoScreen from "./features/demo-screen";
+import ComponentsScreen from "./features/components-screen";
+import { CheckAuth, LoginPage } from "./components/auth";
+import Demo2Screen from "./features/demo2-screen";
 const MODES = {
-    DARK: 'DARK',
-    LIGHT: 'LIGHT'
-}
+  DARK: "DARK",
+  LIGHT: "LIGHT",
+};
 
-IconLoader.addIcons('icon:core/common', common)
-
+IconLoader.addIcons("icon:core/common", common);
 
 function App() {
-  const {mode} = useMobileConfigs()
-
+  const { mode } = useMobileConfigs();
+  console.log(process.env);
   return (
     <ThemeProvider theme={themeColors[MODES.LIGHT]}>
-    <div className="App">
-      <Router baseName='/'>
-      <Switch>
-        <Route exact path='/' component={DemoScreen} />
-        <Route exact path='/components' component={ComponentsScreen} />
-      </Switch>
-      </Router>
-    </div>
+      <div className="App">
+        <Router baseName="/">
+          {/* <CheckAuth /> */}
+          <Switch>
+            <Route exact path="/demo" component={DemoScreen} />
+            <Route exact path="/components" component={ComponentsScreen} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/demo2" component={Demo2Screen} />
+          </Switch>
+        </Router>
+      </div>
     </ThemeProvider>
   );
 }
